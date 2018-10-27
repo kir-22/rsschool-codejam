9d4d618e12de0196a0dcb567f797c8f5e91f2656
kir-22 <kirill89kireew@gmail.com>
2018-10-28 00:59:15 +0300

    feat: create a function make

---
+++ make.js
@@ -0,0 +1,17 @@
+const sum = (a,b) => a+b;
+
+const make = (...args)=>{
+  let val = [];
+  val.push(args.reduce(sum));
+  function f(...args){
+    if(typeof args[0] === 'function'){
+      val = val.reduce(sum);
+    }
+    else{
+      val = val.concat(args);
+    }
+    return f;
+  }
+  f.toString = function(){return val;}
+  return f;
+}
