9ca4fb858c5168b2c804c2f39d35fb675acad048
kir-22 <kirill89kireew@gmail.com>
2018-10-28 15:51:55 +0300

    feat: create a test for function make

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 Object.freeze(assert);
 const sumOfOther = require('./sumOfOther');
-
+const make = require('./make');
 
 describe('sumOfOther', () =>{
   it('1',()=>{
@@ -15,4 +15,14 @@ describe('sumOfOther', () =>{
     assert.deepEqual(array, [0,5,5,5]);
   })
 })
-
+describe('make', ()=>{
+  const sum = (a,b) => a+b;
+  it('1',()=>{
+    const res=make(15)(34, 21, 666)(41)(sum);
+    assert.equal(res,777);
+  })
+  it('2',()=>{
+    const res=make(1)(2, 3, 4)(5)(sum);
+    assert.equal(res,15);
+  })
+})
