ba2739e7347f7f16cbeb37f27a4fd6a57d913ae2
kir-22 <kirill89kireew@gmail.com>
2018-10-28 20:04:29 +0300

    feat: add function recursion

--- recursion.js
+++ recursion.js
@@ -1,1 +1,21 @@
-const recursion = function(){}
+let recursion = function(tree){
+  if(!tree) return [];
+  let array = [];
+
+  search(tree, 1);
+  function search(tree, lvl){
+    if(tree){
+      if(array.length < lvl) array.push([]);
+    
+      let arr=array[lvl-1];
+      arr.push(tree.value);
+      search(tree.left, lvl+1);
+      search(tree.right, lvl+1);
+    }
+    else{
+      return;
+    }
+  }
+  return array;
+}
+module.exports = recursion;
