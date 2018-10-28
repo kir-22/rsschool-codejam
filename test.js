44d280593178d4a0ff8a667d54d3b679bd9038cb
kir-22 <kirill89kireew@gmail.com>
2018-10-28 20:05:48 +0300

    feat: add test for recursion function

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 Object.freeze(assert);
 const sumOfOther = require('./sumOfOther');
 const make = require('./make');
+const recursion = require('./recursion');
 
 describe('sumOfOther', () =>{
   it('1',()=>{
@@ -24,5 +25,27 @@ describe('make', ()=>{
   it('2',()=>{
     const res=make(1)(2, 3, 4)(5)(sum);
     assert.equal(res,15);
+  })
+})
+describe('recursion', ()=>{
+  it('1',()=>{
+    let tree = {
+      "value":100,
+      "left":{
+          "value":90,
+          "left":{
+              "value":70},
+          "right":{
+              "value":99}
+          },
+      "right":{
+          "value":120,
+          "left":{
+              "value":110},
+          "right":{
+              "value":130}
+          }
+      };
+    assert.deepEqual(recursion(tree),[[100], [90, 120], [70,99,110,130]]);
   })
 })
